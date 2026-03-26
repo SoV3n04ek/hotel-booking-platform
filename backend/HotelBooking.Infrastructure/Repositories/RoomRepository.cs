cfdd3e3960e70c5340029ff8a6a84bd3649c46f7
SoV3h04ek <mr.ivan121212@gmail.com>
2026-03-26 23:00:56 +0100

    Added GetAvailableRoomsAsync to RoomRepository

---
+++ backend/HotelBooking.Infrastructure/Repositories/RoomRepository.cs
@@ -0,0 +1,19 @@
+﻿using HotelBooking.Application.Interfaces;
+using HotelBooking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Infrastructure.Repositories;
+
+public class RoomRepository : GenericRepository<Hotel>, IHotelRepository
+{
+    public RoomRepository(HotelsDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<Room>> GetAvailableRoomsAsync(int hotelId, DateTimeOffset start, DateTimeOffset end)
+    {
+        return await _context.Rooms
+             .Where(r => r.HotelId == hotelId)
+             .Where(r => !r.Bookings.Any(b =>
+                 b.DateCheckIn < end && b.DateCheckOut > start))
+             .ToListAsync();
+    }
+}
